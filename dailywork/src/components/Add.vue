6f66b10356be744179578770cd0b2ad6cf6a3d35
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-04-07 16:41:07 +0530

    addition

---
+++ dailywork/src/components/Add.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Add function</h1>
+        <input  type="number" id="num1">
+        <input  type="number" id="num2">
+        <button @click="add()">addition</button>
+    </div>
+</template>
+
+<script>
+export default{
+    name:"AddS",
+    data(){
+        return {  
+        }},
+    methods:{
+        add(){
+            let x=Number(document.getElementById("num1").value);
+             let y = Number(document.getElementById("num2").value);
+             
+             var z=x+y;
+             document.write(z);
+        }
+    }
+}
+</script>
