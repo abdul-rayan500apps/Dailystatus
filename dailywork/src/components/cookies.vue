387397a426939e4e358ab722abf7199b4bb6bfc6
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-04-07 16:41:16 +0530

    cookies

---
+++ dailywork/src/components/cookies.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>vue cookies</h1>
+    <button @click="setCookie">set Cookie</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "CookiesTest",
+  methods: {
+    setCookie() {
+      //console.log("cookies", this.$cookies)
+      //it sets the cookie called 'username'
+      this.$cookies.set("username", "abdulrayan", "1d");
+      //this.$cookies.get("username"); // abdulrayan
+      //console.log("cookies", this.$cookies.get('username'))
+    },
+  },
+};
+</script>
