b48f04345b46ca4ab7d83a0ef2a5696fb8fddaba
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-04-07 16:41:28 +0530

    btable

---
+++ dailywork/src/components/Table.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <b-table :items="items" :fields="fields"></b-table>
+  </div>
+</template>
+<script>
+export default {
+  name: "BTable",
+  components: {},
+  data() {
+    return {
+      fields: [
+        { key: "name", label: "Name" },
+        { key: "department", label: "Department" },
+        { key: "age", label: "Age" },
+        { key: "dateOfBirth", label: "Date Of Birth" },
+      ],
+
+      items: [
+        {
+          name: "Abdul rayan",
+          department: "engineering",
+          age: 24,
+          dateofbirth: "01/05/1998",
+        },
+        {
+          name: "naresh",
+          department: "telecomeng",
+          age: 26,
+          dateofbirth: "29/03/1996",
+        },
+        {
+          name: "rohan",
+          department: "civileng",
+          age: 26,
+          dateofbirth: "31/01/1996",
+        },
+      ],
+    };
+  },
+};
+</script>
