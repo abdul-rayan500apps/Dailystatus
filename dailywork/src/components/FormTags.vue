ceb9937b53adb165a26f1197d46fce5c4082a8c8
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-04-08 15:46:41 +0530

    commit

---
+++ dailywork/src/components/FormTags.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <label for="tags-basic">Type a new tag and press enter</label>
+    <b-form-tags input-id="tags basic" v-model="value"></b-form-tags>
+    <p class="mt-2">Value:{{ value }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"FormTags",
+  data() {
+    return {
+      value: ["orange","red","blue","black","white"],
+    };
+  },
+};
+</script>
