f8d46f049c4899b0a8b4f69b6398817cc86d8915
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-04-08 15:45:28 +0530

    qstn7

---
+++ dailywork/src/components/Question7.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Question no 7</h1>
+
+    {{ txt }}
+  </div>
+</template>
+
+<script>
+var axios = require("axios").default;
+
+export default {
+  name: "QuestioN7",
+
+  data() {
+    return {
+      txt: "",
+    };
+  },
+
+  mounted() {
+    axios
+
+      .get("https://agilecrm.com/")
+
+      .then((response) => (this.txt = response));
+  },
+};
+</script>
+
