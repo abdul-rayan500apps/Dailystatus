094da4c9529de21c29b96b95b8eb5a6f2e6da0a1
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-04-11 18:59:49 +0530

    q6

--- dailywork/src/components/Question6.vue
+++ dailywork/src/components/Question6.vue
@@ -1,58 +1,57 @@
 <template>
   <div>
-    <b-dropdown id="dropdown-1" text="Country" class="m-md-2">
-      <b-dropdown-item @click="India()">India</b-dropdown-item>
-
-      <b-dropdown-item @click="srilanka()">srilanka</b-dropdown-item>
-
-      <b-dropdown-item @click="us()">us</b-dropdown-item>
-    </b-dropdown>
+    <h1>Question 6</h1>
+    <b-form-select v-model="value" :options="countries"></b-form-select>
+    <b-button variant="primary" @click="getData">Get Data</b-button>
+    <b-card v-for="value in items" :key="value.domains" class="mb-2">
+      <b-card-text>university_name:-- {{ value.university_name }} </b-card-text>
+      <p>domains:--{{ value.domains }}</p>
+      <p @click="redirect(value.website_url)">
+        website_url:--{{ value.website_url }}
+      </p>
+      <p>state_province:--{{ value.state_province }}</p>
+    </b-card>
   </div>
 </template>
-
 <script>
+const { getNames } = require("country-list");
 export default {
   name: "QuestioN6",
-
   data() {
     return {
-      countrydet: [
-        { id: 1, name: "India" },
-
-        { id: 2, name: "srilanka" },
-
-        { id: 3, name: "us" },
-
-        { id: 4, name: "us" },
-
-        { id: 5, name: "India" },
-      ],
+      value: "",
+      items: [],
+      countries: [],
     };
   },
-
+  mounted() {
+    let countries = getNames();
+    this.countries = countries.map((row) => {
+      return { value: row, text: row };
+    });
+    if (this.countries.length) this.value = this.countries[0].text;
+  },
   methods: {
-    India() {
-      let country = this.countrydet.map((arrItem) => {
-        if (arrItem.name == "India") return arrItem;
-      });
-
-      console.log(country);
+    redirect(value) {
+      window.open(value, "_blank");
     },
-
-    srilanka() {
-      let country = this.countrydet.map((arrItem) => {
-        if (arrItem.name == "srilanka") return arrItem;
+    async getData() {
+      // Get the response
+      const response = await fetch(
+        "http://universities.hipolabs.com/search?country=" + this.value,
+        {
+          method: "GET",
+        }
+      );
+      const responseText = await response.json();
+      this.items = responseText.map((row) => {
+        return {
+          university_name: row.name,
+          domains: row.domains,
+          website_url: row.web_pages[0],
+          state_province: row["state-province"],
+        };
       });
-
-      console.log(country);
-    },
-
-    us() {
-      let country = this.countrydet.map((arrItem) => {
-        if (arrItem.name == "us") return arrItem;
-      });
-
-      console.log(country);
     },
   },
 };
