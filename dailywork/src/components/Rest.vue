7abeb2d1851035b45ea1d8cc6b088f8e17b28153
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-04-18 19:27:45 +0530

    restapi

---
+++ dailywork/src/components/Rest.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div v-for="post in posts" v-bind:key="post.id">
+      <h2>{{ post.title }}</h2>
+      <p>{{ post.body }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"RestApi",
+  data() {
+    return {
+      posts: [],
+    };
+  },
+
+  methods: {
+    async getData() {
+      try {
+        const response = await this.$http.get(
+          "http://jsonplaceholder.typicode.com/posts"
+        );
+        this.posts = response.data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+
+  created() {
+    this.getData();
+  },
+};
+</script>
