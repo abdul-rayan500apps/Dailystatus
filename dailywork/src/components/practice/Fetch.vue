2837a92d293d41d238a0efe82648660d89c95bd0
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-04-19 19:21:48 +0530

    FETCH

---
+++ dailywork/src/components/practice/Fetch.vue
@@ -0,0 +1,25 @@
+<template>
+<div id="fetch">
+       <h1>{{text}}</h1>
+</div>
+   
+</template>
+
+<script>
+import axios from 'axios';
+
+export default{
+    name:"GetRequest",
+    data(){
+        return{
+            text:null,
+        };   
+    },
+    mounted () {
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => (this.info = response))
+  }
+}
+
+</script>
