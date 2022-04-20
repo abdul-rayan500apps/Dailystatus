5ddb0411e5b5a56e99832ed87196ad0c3bd782de
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-04-20 16:30:23 +0530

    as8q2

---
+++ dailywork/src/components/Assignment-8(20apr)/Qstn2.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>Question-2</h2>
+        <h2>slots:</h2>
+        <p>slot is a reserved space which is used to display content from one component to another.slots are 2 types</p><br>
+        <p>1)Named slot</p><br>
+        <p>2)unnamed slot or default slot</p>
+    </div>
+</template>
+<script>
+
+export default{
+    name:"QstN2",
+    data(){
+        return{
+            
+        }
+    }
+}
+</script>
