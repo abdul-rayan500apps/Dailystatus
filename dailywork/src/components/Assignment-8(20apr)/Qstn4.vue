3321cef574fe1f722964a688d34f1adc9d1e1807
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-04-20 19:12:07 +0530

    qs4

--- dailywork/src/components/Assignment-8(20apr)/Qstn4.vue
+++ dailywork/src/components/Assignment-8(20apr)/Qstn4.vue
@@ -1,66 +1,26 @@
 <template>
-  <div>
-    <h2>Creating APIs</h2>
-
-    <div>
-      <b-form-input v-model="text" placeholder="Enter your name"></b-form-input>
-    </div>
-
-    <b-button @click="getData()">Click Me!</b-button>
-
-    <div>
-      <!--{{ searchData.data }}-->
-    </div>
-    <div>
-      <center>
-        <table border="2px">
-          <tr>
-            <th>Name</th>
-            <th>Num</th>
-            <th>From</th>
-            <th>To</th>
-          </tr>
-          <tbody>
-            <tr v-for="data in searchData.data" :key="data.id">
-              <td>{{ data.name }}</td>
-              <td>{{ data.train_num }}</td>
-              <td>{{ data.train_from }}</td>
-              <td>{{ data.train_to }}</td>
-            </tr>
-          </tbody>
-        </table>
-      </center>
-    </div>
+  <div id="app">
+     
+    <ul>
+      <li v-for="user in users" :key="user.id">
+         {{user.name}}
+         {{user.email}}
+         {{user._id}}
+       </li>
+    </ul>
   </div>
 </template>
 
 <script>
-var axios = require("axios").default;
+import usersData from '@/components/Assignment-8(20apr)/users.json'
+
 export default {
-  name: "QstN4",
+    name:"QstN11",
   data() {
     return {
-      options: {
-        method: "GET",
-        url: "http://www.json-generator.com/api/json/get/bSldINskRK",
-        friends: [
-             '{{repeat(3)}}',
-             {
-                id: '{{index()}}',
-               name: '{{firstName()}} {{surname()}}' 
-             }
-        ],
-        data: { search: "" },
-      },
-      text: "",
-      searchData: '',
+      users: usersData,
+      
     };
-  },
-  methods: {
-    async getData() {
-      this.options.data.search = this.text;
-      this.searchData = await axios.request(this.options);
-    },
   },
 };
 </script>
