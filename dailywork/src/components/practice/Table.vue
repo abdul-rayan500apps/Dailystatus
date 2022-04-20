770e78dfd2cdafef4c2ef75025a80371efa9d85b
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-04-20 19:12:15 +0530

    table

---
+++ dailywork/src/components/practice/Table.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <b-table striped hover :items="items"></b-table>
+    </div>
+</template>
+<script>
+
+export default {
+    name:"TablE",
+    data(){
+        return{
+            fields:["Email","id","Name"],
+            items:[
+                {email:"rayan.abdul53@gmail.com",id:1,name:"abdul rayan"},
+                {email:"abdul-rayan@500apps.com",id:2,name:"abdul hannan"},
+                {email:"abdulrayan@yahoo.com",id:3,name:"abdul mannan"},
+            ]
+        }
+    }
+}
+</script>
