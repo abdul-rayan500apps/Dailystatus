44f2658c47a5da771f2a901fc0f6816d6f9501d3
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-04-25 17:08:52 +0530

    qst3

---
+++ dailywork/src/components/Assignment9(25-apr)/Qst2.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Question no 2</h1>
+    <h1>name:"agile CRM"</h1>
+    <h1>description:"A company of excellence"</h1>
+    <h1>yearsofexperience:"12"</h1>
+    <h1>location:"Hyderabad"</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "QsT2",
+  data() {
+    return {
+      name: "agile CRM",
+      description: "A company of excellence",
+      yearsofexperience: "12",
+      location: "Hyderabad",
+    };
+  },
+};
+</script>
