7dbae232b16052d72869480a9ec8944eac3266e5
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-04-25 17:09:14 +0530

    qst4

---
+++ dailywork/src/components/Assignment9(25-apr)/Qst4parent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Home component</h1>
+        <!--<UserS />-->
+        <ProducT />
+    </div>
+</template>
+<script>
+//import  UserS from '@/components/user';
+import ProducT from '@/components/Assignment9(25-apr)/Qst4child'
+export default {
+    name:"QsT4",
+    components:{
+        //UserS,
+        ProducT
+        
+    }
+}
+</script>
