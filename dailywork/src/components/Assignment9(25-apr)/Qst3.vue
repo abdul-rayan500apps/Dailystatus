febb5f45cb2648cf332b52be1d640fa89a4788b8
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-04-25 17:09:00 +0530

    qst3

---
+++ dailywork/src/components/Assignment9(25-apr)/Qst3.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Question no 3</h1>
+        <Demo1/>
+        <Demo2/>
+    </div>
+</template>
+<script>
+import  Demo1 from '...///' /// this is path for component
+import  Demo2 from '...///' /// this is path for component
+
+export default {
+    name:"Name" ///this is component name
+   // component:
+   //Demo1, /THIS 
+   //Demo2
+}
+</script>
