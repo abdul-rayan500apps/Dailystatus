577627e233cddb6089baef33012afa42128ea7c1
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-04-26 19:15:22 +0530

    commit

---
+++ dailywork/src/components/practice/forloop.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>display a text five times</h1>
+    <button @click="fun()">click on it</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "ForLoop",
+  data() {
+    return {};
+  },
+  methods: {
+    fun() {
+      const n = 5;
+      for (let i = 1; i <= n; i++) {
+        console.log("i love india");
+      }
+    },
+  },
+};
+</script>
