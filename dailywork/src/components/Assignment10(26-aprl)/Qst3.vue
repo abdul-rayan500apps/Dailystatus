1a272b2bd25c9f77365137af8a65cccf5852801c
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-04-26 19:14:48 +0530

    qst3

---
+++ dailywork/src/components/Assignment10(26-aprl)/Qst3.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h3>Question no 3</h3>
+        <b-table items="Names" :fields="fields"></b-table>
+        <b-form-input id="id1" v-model="text" placeholder="Enter first name"></b-form-input> 
+        <b-form-input id="id2" v-model="text" placeholder="Enter last name"></b-form-input>
+        <button @click="add()">Add button</button>
+        <button @click="remove()">Deletebutton</button>
+    </div>
+</template>
+<script>
+
+
+export default {
+    name:"QsTN3",
+    data(){
+        return{
+            Names:[{firstname:"Agile",lastname:"CRM"}],
+            
+        }
+    },
+    methods:{
+        add(){
+            this.Names.push({firstname:(document.getElementById("id1").value),lastname:(document.getElementById("id2").value)});
+       },
+       remove(){
+           this.Names.pop()
+       }
+    }
+}
+</script>
