0d3c0259fbd9dc55c8c9611bead0a6766ecc7129
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-04-26 19:14:41 +0530

    qst2

---
+++ dailywork/src/components/Assignment10(26-aprl)/Qst2.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <h1>Question no2</h1>
+        <form name="myform">
+            <label for="" name="num1">1st number</label>
+            <input type="text" name="num1" id="id1"><br>
+            <label for="" name="num2">2nd number</label>
+            <input type="text" name="num2" id="id2"><br>
+            <button @click="mul()">multiply</button>
+            <button @click="div()">divide</button>
+            <h5>the result is :</h5>
+            <p id="id3"></p>
+        </form>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    name:"QsTN2",
+    data(){
+        return{
+         z:"",
+        }
+    },
+    methods:{
+        mul(){
+            var x = document.getElementById("id1").value
+            var y = document.getElementById("id2").value
+            this.z=Number(x*y)
+            document.getElementById("id3").innerHTML=this.z
+            console.log(this.z);
+        },
+        div(){
+         var x = document.getElementById("id1").value
+            var y = document.getElementById("id2").value
+            this.z=Number(x/y)
+            document.getElementById("id3").innerHTML=this.z
+            console.log(this.z)
+            
+        }
+    }
+}
+</script>
