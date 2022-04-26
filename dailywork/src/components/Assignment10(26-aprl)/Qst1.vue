d05e758e8425bfee76748f44eec7e9c35f20197a
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-04-26 19:14:33 +0530

    qst1

---
+++ dailywork/src/components/Assignment10(26-aprl)/Qst1.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <button @click="func()">Click</button>
+    <p>{{ users }}</p>
+    <p id="id"></p>
+  </div>
+</template>
+<script>
+export default {
+  name: "QsTN1",
+  data() {
+    return {
+      users: [
+        { name: "500apps", id: 1 },
+        { name: "Technologies", id: 2 },
+        { name: "Agile", id: 3 },
+        { name: "Mantra", id: 4 },
+        { name: "Clockly", id: 5 },
+        { name: "CRM", id: 6 },
+        { name: "500apps", id: 7 },
+      ],
+      reverse1: "",
+      
+    };
+  },
+  methods: {
+    func() {
+      this.reverse1 = this.users.reverse(); 
+    },
+    },
+    
+};
+</script>
