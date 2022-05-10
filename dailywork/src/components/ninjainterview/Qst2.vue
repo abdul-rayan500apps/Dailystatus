f140e3e68fc291fb95b0c3c78d4b3434badb26e4
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-05-10 19:22:22 +0530

    qst2

---
+++ dailywork/src/components/ninjainterview/Qst2.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <b-form-input
+      v-model="lower"
+      type="text"
+      placeholder="lower"
+    ></b-form-input>
+    <b-form-input
+      v-model="upper"
+      type="text"
+      placeholder="upper"
+    ></b-form-input>
+  </div>
+</template>
+<script>
+export default {
+  name: "QSTn2",
+  data() {
+    return {
+      lower: "",
+      upper: "",
+    };
+  },
+
+  watch: {
+    lower: function (val) {
+      this.lower = val;
+      this.upper = this.lower.toUpperCase();
+    },
+  },
+};
+</script>
