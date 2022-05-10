61cd4da639bed8f4c2d7567f87f194d465a4c84b
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-05-10 19:48:19 +0530

    Qs1

--- dailywork/src/components/ninjainterview/Qst1.vue
+++ dailywork/src/components/ninjainterview/Qst1.vue
@@ -1,21 +1,72 @@
 <template>
   <div>
-    <b-table striped hover :items="items"></b-table>
+    <b-form id="form">
+      Enter product name:<b-form-input
+        id="name"
+        class="name"
+        placeholder="Enter item name"
+      ></b-form-input>
+
+      price:<b-form-input
+        id="price"
+        class="price"
+        placeholder="Enter item price:"
+      ></b-form-input>
+
+      Enter product category:<b-form-input
+        id="category"
+        class="categories"
+        placeholder="Enter item category"
+      ></b-form-input>
+
+      <b-button @click="Add()">Add</b-button><br /><br />
+    </b-form>
+
+    <b-table striped hover :items="items" :fields="fields"></b-table>
   </div>
 </template>
 
 <script>
-  export default {
-      name:"QSTn1",
-    data() {
-      return {
-        items: [
-          { productname: "amulcooldrink", price: "100rs", productcategory: 'Dairyproducts' },
-          { productname: "pepsi", price: '72rs', productcategory: 'Dairyproducts' },
-          { productname: "milk", price: '45rs', productcategory: 'Dairyproducts' },
-          { productname: "eggs", price: '150rs', productcategory: 'Dairyproducts' }
-        ]
-      }
-    }
-  }
+export default {
+  name: "QSTn1",
+  data() {
+    return {
+      items: [
+        {
+          productname: "amulcooldrink",
+          price: "100rs",
+          productcategory: "Dairyproducts",
+        },
+        {
+          productname: "pepsi",
+          price: "72rs",
+          productcategory: "Dairyproducts",
+        },
+        {
+          productname: "milk",
+          price: "45rs",
+          productcategory: "Dairyproducts",
+        },
+        {
+          productname: "eggs",
+          price: "150rs",
+          productcategory: "Dairyproducts",
+        },
+      ],
+
+      fields: [],
+    };
+  },
+  methods: {
+    Add() {
+      this.items.push({
+        productname: document.getElementById("name").value,
+
+        price: document.getElementById("price").value,
+
+        productcategory: document.getElementById("category").value,
+      });
+    },
+  },
+};
 </script>
