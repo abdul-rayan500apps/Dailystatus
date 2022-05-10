29ca7a2dbaeb4b855ce27d1ced83344ce0bcbc5a
abdul-raya500apps <abdul.rayan+1@mantragroup.com>
2022-05-10 17:15:02 +0530

    NINJAINTERVIEW

---
+++ dailywork/src/components/ninjainterview/Qst1.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <b-table striped hover :items="items"></b-table>
+  </div>
+</template>
+
+<script>
+  export default {
+      name:"QSTn1",
+    data() {
+      return {
+        items: [
+          { productname: "amulcooldrink", price: "100rs", productcategory: 'Dairyproducts' },
+          { productname: "pepsi", price: '72rs', productcategory: 'Dairyproducts' },
+          { productname: "milk", price: '45rs', productcategory: 'Dairyproducts' },
+          { productname: "eggs", price: '150rs', productcategory: 'Dairyproducts' }
+        ]
+      }
+    }
+  }
+</script>
